24def236140312b3f74045614ffe525d07f3cfe4
Demetrius <63015481+dimagela29@users.noreply.github.com>
2022-02-16 11:49:10 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -14,35 +14,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from pyspark.pandas.exceptions import PandasNotImplementedError
-
-
-def unsupported_function(class_name, method_name, deprecated=False, reason=""):
-    def unsupported_function(*args, **kwargs):
-        raise PandasNotImplementedError(
-            class_name=class_name, method_name=method_name, reason=reason
-        )
-
-    def deprecated_function(*args, **kwargs):
-        raise PandasNotImplementedError(
-            class_name=class_name, method_name=method_name, deprecated=deprecated, reason=reason
-        )
-
-    return deprecated_function if deprecated else unsupported_function
-
-
-def unsupported_property(class_name, property_name, deprecated=False, reason=""):
-    @property
-    def unsupported_property(self):
-        raise PandasNotImplementedError(
-            class_name=class_name, property_name=property_name, reason=reason
-        )
-
-    @property
-    def deprecated_property(self):
-        raise PandasNotImplementedError(
-            class_name=class_name, property_name=property_name, deprecated=deprecated, reason=reason
-        )
-
-    return deprecated_property if deprecated else unsupported_property
--- functions.py
+++ functions.py
@@ -14,117 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""
+Additional Spark functions used in pandas-on-Spark.
+"""
+from typing import Any, Union, no_type_check
+
+import numpy as np
 
 from pyspark import SparkContext
-from pyspark.sql.column import Column, _to_java_column
+from pyspark.sql import functions as F
+from pyspark.sql.column import Column, _to_java_column, _create_column_from_literal  # type: ignore
+from pyspark.sql.types import (
+    ByteType,
+    FloatType,
+    IntegerType,
+    LongType,
+)
 
 
-def vector_to_array(col, dtype="float64"):
+def repeat(col: Column, n: Union[int, Column]) -> Column:
     """
-    Converts a column of MLlib sparse/dense vectors into a column of dense arrays.
-
-    .. versionadded:: 3.0.0
-
-    Parameters
-    ----------
-    col : :py:class:`pyspark.sql.Column` or str
-        Input column
-    dtype : str, optional
-        The data type of the output array. Valid values: "float64" or "float32".
-
-    Returns
-    -------
-    :py:class:`pyspark.sql.Column`
-        The converted column of dense arrays.
-
-    Examples
-    --------
-    >>> from pyspark.ml.linalg import Vectors
-    >>> from pyspark.ml.functions import vector_to_array
-    >>> from pyspark.mllib.linalg import Vectors as OldVectors
-    >>> df = spark.createDataFrame([
-    ...     (Vectors.dense(1.0, 2.0, 3.0), OldVectors.dense(10.0, 20.0, 30.0)),
-    ...     (Vectors.sparse(3, [(0, 2.0), (2, 3.0)]),
-    ...      OldVectors.sparse(3, [(0, 20.0), (2, 30.0)]))],
-    ...     ["vec", "oldVec"])
-    >>> df1 = df.select(vector_to_array("vec").alias("vec"),
-    ...                 vector_to_array("oldVec").alias("oldVec"))
-    >>> df1.collect()
-    [Row(vec=[1.0, 2.0, 3.0], oldVec=[10.0, 20.0, 30.0]),
-     Row(vec=[2.0, 0.0, 3.0], oldVec=[20.0, 0.0, 30.0])]
-    >>> df2 = df.select(vector_to_array("vec", "float32").alias("vec"),
-    ...                 vector_to_array("oldVec", "float32").alias("oldVec"))
-    >>> df2.collect()
-    [Row(vec=[1.0, 2.0, 3.0], oldVec=[10.0, 20.0, 30.0]),
-     Row(vec=[2.0, 0.0, 3.0], oldVec=[20.0, 0.0, 30.0])]
-    >>> df1.schema.fields
-    [StructField(vec,ArrayType(DoubleType,false),false),
-    StructField(oldVec,ArrayType(DoubleType,false),false)]
-    >>> df2.schema.fields
-    [StructField(vec,ArrayType(FloatType,false),false),
-    StructField(oldVec,ArrayType(FloatType,false),false)]
+    Repeats a string column n times, and returns it as a new string column.
     """
-    sc = SparkContext._active_spark_context
-    return Column(
-        sc._jvm.org.apache.spark.ml.functions.vector_to_array(_to_java_column(col), dtype))
+    sc = SparkContext._active_spark_context  # type: ignore
+    n = _to_java_column(n) if isinstance(n, Column) else _create_column_from_literal(n)
+    return _call_udf(sc, "repeat", _to_java_column(col), n)
 
 
-def array_to_vector(col):
+def lit(literal: Any) -> Column:
     """
-    Converts a column of array of numeric type into a column of pyspark.ml.linalg.DenseVector
-    instances
-
-    .. versionadded:: 3.1.0
-
-    Parameters
-    ----------
-    col : :py:class:`pyspark.sql.Column` or str
-        Input column
-
-    Returns
-    -------
-    :py:class:`pyspark.sql.Column`
-        The converted column of dense vectors.
-
-    Examples
-    --------
-    >>> from pyspark.ml.functions import array_to_vector
-    >>> df1 = spark.createDataFrame([([1.5, 2.5],),], schema='v1 array<double>')
-    >>> df1.select(array_to_vector('v1').alias('vec1')).collect()
-    [Row(vec1=DenseVector([1.5, 2.5]))]
-    >>> df2 = spark.createDataFrame([([1.5, 3.5],),], schema='v1 array<float>')
-    >>> df2.select(array_to_vector('v1').alias('vec1')).collect()
-    [Row(vec1=DenseVector([1.5, 3.5]))]
-    >>> df3 = spark.createDataFrame([([1, 3],),], schema='v1 array<int>')
-    >>> df3.select(array_to_vector('v1').alias('vec1')).collect()
-    [Row(vec1=DenseVector([1.0, 3.0]))]
+    Creates a Column of literal value.
     """
-    sc = SparkContext._active_spark_context
-    return Column(
-        sc._jvm.org.apache.spark.ml.functions.array_to_vector(_to_java_column(col)))
+    if isinstance(literal, np.generic):
+        scol = F.lit(literal.item())
+        if isinstance(literal, np.int64):
+            return scol.astype(LongType())
+        elif isinstance(literal, np.int32):
+            return scol.astype(IntegerType())
+        elif isinstance(literal, np.int8) or isinstance(literal, np.byte):
+            return scol.astype(ByteType())
+        elif isinstance(literal, np.float32):
+            return scol.astype(FloatType())
+        else:  # TODO: Complete mappings between numpy literals and Spark data types
+            return scol
+    else:
+        return F.lit(literal)
 
 
-def _test():
-    import doctest
-    from pyspark.sql import SparkSession
-    import pyspark.ml.functions
-    import sys
-    globs = pyspark.ml.functions.__dict__.copy()
-    spark = SparkSession.builder \
-        .master("local[2]") \
-        .appName("ml.functions tests") \
-        .getOrCreate()
-    sc = spark.sparkContext
-    globs['sc'] = sc
-    globs['spark'] = spark
-
-    (failure_count, test_count) = doctest.testmod(
-        pyspark.ml.functions, globs=globs,
-        optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
-    spark.stop()
-    if failure_count:
-        sys.exit(-1)
+@no_type_check
+def _call_udf(sc, name, *cols):
+    return Column(sc._jvm.functions.callUDF(name, _make_arguments(sc, *cols)))
 
 
-if __name__ == "__main__":
-    _test()
+@no_type_check
+def _make_arguments(sc, *cols):
+    java_arr = sc._gateway.new_array(sc._jvm.Column, len(cols))
+    for i, col in enumerate(cols):
+        java_arr[i] = col
+    return java_arr
